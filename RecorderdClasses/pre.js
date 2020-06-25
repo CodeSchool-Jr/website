687291cb5a31cb1e4a794d05e425a25e21fd18be
Avinash Kumar <bhaiavinash9@gmail.com>
2020-06-26 01:57:43 +0530

    PerviousClassesFolder

---
+++ RecorderdClasses/pre.js
@@ -0,0 +1,16 @@
+$(function(){
+
+
+    $('.multiple-items').slick({
+      prevArrow:".previewx",
+      nextArrow:".forwardx ",
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows:true,
+        autoplay:true,
+        autoplaySpeed:2000,
+        dots:false,
+        centerMode: true,
+        centerPadding: '40px',
+      });});
